87053dddde6b0abc9625122d507af03540e150b6
Eoghan Maxwell <eoghan.maxwell@kiwi.com>
2020-12-17 16:19:54 +0000

    chore: add eslint and prettier

--- src/handlers/usersLambdaFunction.js
+++ src/handlers/usersLambdaFunction.js
@@ -1,26 +1,26 @@
-const { Client } = require('pg')
-const dotenv = require('dotenv');
+const { Client } = require("pg");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-exports.default = async (event) => {
+exports.default = async () => {
   let res;
-    try {
-        const client = new Client()
-        await client.connect()
+  try {
+    const client = new Client();
+    await client.connect();
 
-        res = await client.query('SELECT * from users')
-        await client.end();
-    } catch (error) {
-        return {
-            statusCode: 400,
-            body: "Error loading users",
-            // error,
-        }
-    }
-
+    res = await client.query("SELECT * from users");
+    await client.end();
+  } catch (error) {
     return {
-        statusCode: 200,
-        body: res.rows,
+      statusCode: 400,
+      body: "Error loading users",
+      // error,
     };
+  }
+
+  return {
+    statusCode: 200,
+    body: res.rows,
+  };
 };
